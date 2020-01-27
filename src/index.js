8c077f488abbcbfc438cc3540d911837688cd365
yuxiang660 <yuxiang660@163.com>
2020-01-27 20:47:54 +0800

    Rename stateHook file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StateHook from './StateHook';
+import StateHook from './stateHook';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<StateHook />, document.getElementById('root'));
